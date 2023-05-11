6beda348c18bc3e16c27cc6029c9e507c3e62286
Rafaeli Rangel <rafaelirangel@icloud.com>
2023-05-11 17:15:09 -0400

    latest push

--- blog/settings.py
+++ blog/settings.py
@@ -137,3 +137,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880
