5bce43f0356611165d9db85c2bb9ca165ba68aba
Rafaeli Rangel <rafaelirangel@icloud.com>
2023-05-13 01:03:09 -0400

    full auth working

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Login(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
-from .views import RegisterAPI
+from .views import RegisterAPI, LoginAPI, UserAPI
 from knox import views as knox_views
 
 urlpatterns = [
     path('auth', include('knox.urls')),
-    path('auth/register', RegisterAPI.as_view(), name='user_register')
+    path('auth/register', RegisterAPI.as_view(), name='user_register'),
+    path('auth/login', LoginAPI.as_view(), name='user_login'),
+    path('auth/user', UserAPI.as_view(), name='user'),
+    path('auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
 ]
+    
