8ffa68d56300670808248e962b4632e743fb8199
Rafaeli Rangel <rafaelirangel@icloud.com>
2023-05-12 22:53:39 -0400

    added restrictions to user actions

--- post/models.py
+++ post/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 import os
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Post(models.Model):
-    post_img = models.ImageField(upload_to='post_images', null=True, blank=True)
+    post_img = models.ImageField(upload_to='post_img', null=True, blank=True)
     title = models.CharField(max_length=50)
     message = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def can_edit(self, user):
+        return user == self.user
     def delete(self, *args, **kwargs):
         # Delete the image file if it exists
         if self.post_img:
             if os.path.isfile(self.post_img.path):
                 os.remove(self.post_img.path)
-        super().delete(*args, **kwargs)
+    def can_delete(self, user):
+        return user == self.user
+    super().delete(*args, **kwargs)
--- post/views.py
+++ post/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
-# Create your views here.
 from post.models import Post
 from rest_framework import viewsets, permissions
 from .serializers import PostSerializer
@@ -8,13 +6,21 @@ from django.http import HttpResponseNotAllowed
 from .models import Post
 # Post views
 
+# User only allowed to update, delete or create a post if he is authenticaded 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-    permission_classes = [
-        permissions.AllowAny
-    ]
     serializer_class = PostSerializer
 
+
+    def get_permissions(self):
+        if self.action == 'create' or self.action == 'update' or self.action == 'destroy':
+            permission_classes = [permissions.IsAuthenticated]
+        else:
+            permission_classes = [permissions.AllowAny]
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
