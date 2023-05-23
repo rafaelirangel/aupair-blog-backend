25a117bb68167f5e88b32dbbf355f5319c5868bc
Nathen <leonard91j@gmail.com>
2023-05-23 13:50:05 -0400

    settings

--- blog/settings.py
+++ blog/settings.py
@@ -39,7 +39,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://0.0.0.0',
     'http://127.0.0.1:3000',
     'https://localhost:3000',
-    'carebeyondborders-front-nateleo91.vercel.app',
+    'https://carebeyondborders-front-nateleo91.vercel.app',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
